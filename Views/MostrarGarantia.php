a6a91d47e91d125566ec04a691d339a1d315903b
fernandareyesc16 <49354126+fernandareyesc16@users.noreply.github.com>
2019-04-11 11:51:49 -0500

    Create MostrarGarantia.php

---
+++ Views/MostrarGarantia.php
@@ -0,0 +1,14 @@
+<?php
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title> Mostrar garantía</title>
+  </head>
+  <body>
+
+      <h1>Garantia</h1>
+    </body>
+  </form>
+  </body>
+  </html>
+?>
