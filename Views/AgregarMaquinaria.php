64378cbd2d9550cd252d5aeaf76cd229e75b1617
NicoleBarra <38593156+NicoleBarra@users.noreply.github.com>
2019-04-11 11:54:18 -0500

    nico here

---
+++ Views/AgregarMaquinaria.php
@@ -0,0 +1,30 @@
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title> Agregar Maquinaria</title>
+  </head>
+  <body>
+      <form name='form' method='post'>
+
+      <div>
+        <h1>Agregar Maquinaria </h1>
+      </div>
+      <div>
+          <input type="text" class="textbox" id="txt_modelo" name="txt_modelo" placeholder="Modelo" />
+          <input type="text" class="textbox" id="txt_" name="txt_apellido" placeholder="Apellidos" />
+      </div>
+      <div>
+          <input type="text" class="textbox" id="txt_correo" name="txt_correo" placeholder="Correo" />
+      </div>
+      <div>
+          <input type="date" class="textbox" id="date_fecha_de_compra" name="date_fecha_de_compra" placeholder="Fecha de Compra" />
+      </div>
+      <div>
+          <input type="submit" name="but_submit" value = "Agregar" />
+      </div>
+    </form>
+
+
+  </body>
+  <html>
