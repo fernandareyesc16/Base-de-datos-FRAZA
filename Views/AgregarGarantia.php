9bed4114ed339847964348e2b72a79ad4e2e47cc
Monica Lara <38739285+itsmonicalara@users.noreply.github.com>
2019-04-11 22:25:25 -0500

    Add files via upload

---
+++ Views/AgregarGarantia.php
@@ -0,0 +1,30 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title> Agregar Garantia</title>
+  </head>
+  <body>
+      <form name='form' method='post'>
+
+      <div>
+        <h1>Agregar Garantia</h1>
+      </div>
+      <div>
+          <input type="text" class="textbox" id="txt_cliente" name="txt_cliente" placeholder="Cliente" />
+          <input type="text" class="textbox" id="txt_fecha_creacion" name="txt_fecha_creacion" placeholder="Fecha de creación" />
+      </div>
+      <div>
+          <input type="text" class="textbox" id="txt_monto_total" name="txt_monto_total" placeholder="Monto total" />
+      </div>
+      <div>
+          <input type="text" class="textbox" id="txt_maquinaria" name="txt_maquinaria" placeholder="Maquinaria" />
+          <input type="text" class="textbox" id="txt_fase" name="txt_fase" placeholder="Fase" />
+      </div>
+      <div>
+          <input type="submit" name="but_submit" value = "Agregar" />
+      </div>
+    </form>
+
+
+  </body>
+  <html>
