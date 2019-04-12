cc97f90ea95206e31de7178e3345f4e0cb70ff4d
jimena <44758705+jimeBB@users.noreply.github.com>
2019-04-11 23:40:04 -0500

    Create logIn.php

---
+++ Views/logIn.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Fraza</title>
+	
+<link href="css/style.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Source+Code+Pro:200,300,400,500,600" rel="stylesheet">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<script src="js/jquery-3.3.1.min.js"> </script>
+</head>
+
+<body>
+	
+	<section id="slide2">
+<form  method="post" name="form"  >
+Username : <input type="text" name="username"><br><br>
+Password : <input type="password" name="password"><br><br>
+<input type="submit" name="but_submit" value="submit" id="login">
+</form>
+	</section>
+    </body>
+  <html>
