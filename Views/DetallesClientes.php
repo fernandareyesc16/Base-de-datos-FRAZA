a867b74d0b17b6dd96466de73d1b3207986af8c2
fernandareyesc16 <49354126+fernandareyesc16@users.noreply.github.com>
2019-04-13 14:25:37 -0500

    Create DetallesClientes.php

---
+++ Views/DetallesClientes.php
@@ -0,0 +1,15 @@
+<?php
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title> Detalles clientes</title>
+  </head>
+  <body>
+
+      <h1>Clientes</h1>
+    </body>
+  </form>
+  </body>
+  </html>
+?>
