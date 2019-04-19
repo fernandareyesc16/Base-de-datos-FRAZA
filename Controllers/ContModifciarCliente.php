883c0ee45f3c4f7eba317b22f9597709f972eedc
Natalia670 <36935166+Natalia670@users.noreply.github.com>
2019-04-19 14:16:14 -0500

    Delete ContModifciarCliente.php

--- Controllers/ContModifciarCliente.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once("../Models/ModificarCliente.php");
-$modificarcliente = new ModificarCliente();
-$row = $modificarcliente->updateDetallesCliente();//llamar update de mysql 
-require_once("../views/ModificarCliente.php");
-
-?>
