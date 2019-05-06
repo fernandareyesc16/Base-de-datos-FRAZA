dd416fc61bd0a24899d276f8a709aae153771c0d
JimeBB <jimeberbau@outlook.es>
2019-05-06 02:44:15 -0500

    css mostrar garantía

--- Views/MostrarGarantia.php
+++ Views/MostrarGarantia.php
@@ -112,7 +112,7 @@ http://www.tooplate.com/view/2082-pure-mix
 
            		</div>
 			</div>
-
+<form action= "../Controllers/ContAgregarCliente.php"><input type="submit" value="Agregar garantía" id="agregar-clientes"></form>
 		</div>
 	</div>
 </section>
